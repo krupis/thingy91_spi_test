c9d454b6f6da2bfe108b39dafe4324ccd97347f1
Lukas Petrikas <lukas.petrikas@teltonika.lt>
2023-11-16 15:03:51 +0200

    change the way gpio is configured

--- src/main.c
+++ src/main.c
@@ -10,16 +10,16 @@
 LOG_MODULE_REGISTER(main);
 
 
-//CS GPIO CONFIG
- #define ADXL362_CS_GPIO_NODE DT_NODELABEL(spi3)
- #define ADXL362_CS_GPIO_PIN 8
- const struct device* gpio_dt_spec = DEVICE_DT_GET(ADXL362_CS_GPIO_NODE);
+// Define the CS pin
+#define CS_PIN 8
+const struct device *gpio_dev;
 
 int main(void)
 {
 	int err;
 	printk("Program started \n");
-	err = gpio_pin_configure(gpio_dt_spec,ADXL362_CS_GPIO_PIN,GPIO_OUTPUT);
+	gpio_pin_configure(gpio_dev, CS_PIN, GPIO_OUTPUT);
+
 	if (err < 0)
 	{
 		printk("GPIO pin configure failed with error %d\n", err);
