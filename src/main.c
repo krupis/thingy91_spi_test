7e3e3b899aa244c5419eb363ddb4c8f463aa499f
Lukas Petrikas <lukas.petrikas@teltonika.lt>
2023-11-16 14:43:46 +0200

    before creating new branch

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-#define SPI_MESSAGE 0x5A
 static int readRegister(uint8_t reg, uint8_t* values, uint8_t size);
 
 #define DEFAULT_ADXL362_NODE DT_ALIAS(adxl362)
@@ -18,9 +17,6 @@ BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_ADXL362_NODE, okay),
 
 // DEVICE TREE STRUCTURE
 const struct device *adxl1362_sens = DEVICE_DT_GET(DEFAULT_ADXL362_NODE);
-
-// CHIP SELECT CONTROL
-struct spi_cs_control ctrl = SPI_CS_CONTROL_INIT(DT_NODELABEL(adxl362), 0);
 
 // SPI CONFIG
 static const struct spi_config spi_cfg = {
@@ -37,48 +33,32 @@ static const struct spi_config spi_cfg = {
 
 int main(void)
 {
+	int err;
 	printk("Program started \n");
-	gpio_pin_configure(cs_gpio_dev,ADXL362_CS_GPIO_PIN,GPIO_OUTPUT);
+	err = gpio_pin_configure(cs_gpio_dev,ADXL362_CS_GPIO_PIN,GPIO_OUTPUT);
+	if (err < 0)
+	{
+		printk("GPIO pin configure failed with error %d\n", err);
+		return 0;
+	}
 	k_msleep(1000);
-	// while(1){
-	// 	printk("Hello world \n");
-	// 	k_msleep(1000);
-	// }
-
 	printk("getting into while loop \n");
+
 	uint8_t values[1];
 	while(1)
 	{
-		int ret = readRegister(0x00,values,1);
-		if (ret == 0)
-		{
-			printk("Read chip ID failed \n");
-			return 0;
-		}
-		printk("Register chip ID: %d\n",values[0]);
+		// int ret = readRegister(0x00,values,1);
+		// if (ret == 0)
+		// {
+		// 	printk("Read chip ID failed \n");
+		// 	return 0;
+		// }
+		// printk("Register chip ID: %d\n",values[0]);
+		// k_msleep(1000);
+
+		printk("Hello world \n");
 		k_msleep(1000);
 	}
-	
-	/*
-	int err;
-	uint8_t cmd = SPI_MESSAGE;
-	struct spi_buf tx_buf = {.buf = &cmd, .len = 1};
-	struct spi_buf_set tx_bufs = {.buffers = &tx_buf, .count = 1};
-	while (1)
-	{
-		LOG_INF("SPI writing test data \n");
-		// gpio_pin_set(cs_gpio_dev,ADXL362_CS_GPIO_PIN,1);
-		err = spi_write(adxl1362_sens, &spi_cfg, &tx_bufs);
-		if (err)
-		{
-			LOG_ERR("SPI write failed with error %d\n", err);
-			return err;
-		}
-		// gpio_pin_set(cs_gpio_dev,ADXL362_CS_GPIO_PIN,0);
-
-		k_sleep(K_MSEC(1000));
-	}
-	*/
 
 	return 0;
 }
