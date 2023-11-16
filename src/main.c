395234a888f0734f7a16f85484784f5c77d29b2c
Lukas Petrikas <lukas.petrikas@teltonika.lt>
2023-11-16 15:22:03 +0200

    test

--- src/main.c
+++ src/main.c
@@ -11,14 +11,18 @@ LOG_MODULE_REGISTER(main);
 
 
 // Define the CS pin
-#define CS_PIN 8
-const struct device *gpio_dev;
+// #define CS_PIN 8
+// const struct device *gpio_dev;
+
+#define MY_GPIO0 DT_NODELABEL(gpio0)
+#define GPIO0_8_CS 8
+const struct device* gpio0_dev = DEVICE_DT_GET(MY_GPIO0);
 
 int main(void)
 {
 	int err;
 	printk("Program started \n");
-	gpio_pin_configure(gpio_dev, CS_PIN, GPIO_OUTPUT);
+	gpio_pin_configure(gpio0_dev, GPIO0_8_CS, GPIO_OUTPUT);
 
 	if (err < 0)
 	{
