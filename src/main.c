8cb243b2d8dfbf50709cd411e653cb0382fa1f6a
Lukas Petrikas <lukas.petrikas@teltonika.lt>
2023-11-17 10:26:11 +0200

    try transceive

--- src/main.c
+++ src/main.c
@@ -9,7 +9,17 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-static int readRegister(uint8_t reg, uint8_t* values, uint8_t size);
+#define ADXL362_WRITE_REG 0x0A
+#define ADXL362_READ_REG 0x0B
+#define ADXL362_READ_FIFO 0x0D
+
+#define ADXL362_REG_DEVID_AD 0x00
+#define ADXL362_REG_DEVID_MST 0x01
+#define ADXL362_REG_PARTID 0x02
+
+
+//static int readRegister(uint8_t reg, uint8_t *values, uint8_t size);
+static int readRegister2(uint8_t reg, uint8_t *values, uint8_t size);
 
 #define DEFAULT_ADXL362_NODE DT_ALIAS(adxl362)
 BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_ADXL362_NODE, okay),
@@ -18,59 +28,109 @@ BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_ADXL362_NODE, okay),
 // DEVICE TREE STRUCTURE
 const struct device *adxl1362_sens = DEVICE_DT_GET(DEFAULT_ADXL362_NODE);
 
+// CS CONTROL
+struct spi_cs_control ctrl = {
+        .gpio = SPI_CS_GPIOS_DT_SPEC_GET(DT_NODELABEL(adxl362)),
+        .delay = 2,
+};
+
 // SPI CONFIG
 static const struct spi_config spi_cfg = {
 	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
 	.frequency = 1000000, // 1 mhz
 	.slave = 0,
-	//.cs = &ctrl,
+	.cs = &ctrl,
 };
 
-//CS GPIO CONFIG
- #define ADXL362_CS_GPIO_NODE DT_NODELABEL(spi3)
- #define ADXL362_CS_GPIO_PIN 8
- const struct device* cs_gpio_dev = DEVICE_DT_GET(ADXL362_CS_GPIO_NODE);
+// CS GPIO CONFIG
+//  #define MY_GPIO0 DT_NODELABEL(gpio0)
+//  #define GPIO0_8_CS 8
+//  const struct device* gpio0_dev = DEVICE_DT_GET(MY_GPIO0);
 
 int main(void)
 {
 	int err;
 	printk("Program started \n");
-	err = gpio_pin_configure(cs_gpio_dev,ADXL362_CS_GPIO_PIN,GPIO_OUTPUT);
-	if (err < 0)
-	{
-		printk("GPIO pin configure failed with error %d\n", err);
-		return 0;
-	}
+	// err = gpio_pin_configure(gpio0_dev,GPIO0_8_CS,GPIO_OUTPUT);
+	//  if (err < 0)
+	//  {
+	//  	printk("GPIO pin configure failed with error %d\n", err);
+	//  	return 0;
+	//  }
+	// gpio_pin_set(gpio0_dev,GPIO0_8_CS,1);
 	k_msleep(1000);
 	printk("getting into while loop \n");
 
 	uint8_t values[1];
-	while(1)
+	while (1)
 	{
-		// int ret = readRegister(0x00,values,1);
-		// if (ret == 0)
-		// {
-		// 	printk("Read chip ID failed \n");
-		// 	return 0;
-		// }
-		// printk("Register chip ID: %d\n",values[0]);
-		// k_msleep(1000);
-
-		printk("Hello world \n");
-		k_msleep(1000);
+		int ret = readRegister2(0x00, values, 1);
+		if (ret == 0)
+		{
+			printk("Read chip ID failed \n");
+			k_msleep(1000);
+		}
+		else
+		{
+			printk("Register chip ID: %d\n", values[0]);
+			k_msleep(1000);
+		}
 	}
 
 	return 0;
 }
 
-static int readRegister(uint8_t reg, uint8_t* values, uint8_t size)
+// static int readRegister(uint8_t reg, uint8_t *values, uint8_t size)
+// {
+// 	int err;
+// 	uint8_t tx_buffer[1];
+// 	tx_buffer[0] = reg;
+
+// 	struct spi_buf tx_spi_buf = {
+// 		.buf = tx_buffer,
+// 		.len = sizeof(tx_buffer)};
+
+// 	struct spi_buf_set spi_tx_buffer_set = {
+// 		.buffers = &tx_spi_buf,
+// 		.count = 1};
+
+// 	struct spi_buf rx_spi_buf = {
+// 		.buf = values,
+// 		.len = size};
+
+// 	struct spi_buf_set spi_rx_buffer_set = {
+// 		.buffers = &rx_spi_buf,
+// 		.count = 1};
+
+// 	//  chip select pin set to low
+// 	gpio_pin_set(gpio0_dev, GPIO0_8_CS, 0);
+// 	err = spi_write(adxl1362_sens, &spi_cfg, &spi_tx_buffer_set);
+// 	if (err < 0)
+// 	{
+// 		printk("SPI write failed with error %d\n", err);
+// 		return 0;
+// 	}
+// 	err = spi_read(adxl1362_sens, &spi_cfg, &spi_rx_buffer_set);
+// 	// chip select pin set to high
+// 	gpio_pin_set(gpio0_dev, GPIO0_8_CS, 1);
+// 	if (err < 0)
+// 	{
+// 		printk("SPI read failed with error %d\n", err);
+// 		return 0;
+// 	}
+// }
+
+//According to the ADXL362 datasheet (https://www.analog.com/media/en/technical-documentation/data-sheets/adxl362.pdf)
+//Figure 36, we follow multi byte structure where first byte is ADXL362_READ_REG and second byte is the register address
+static int readRegister2(uint8_t reg, uint8_t *values, uint8_t size)
 {
 	int err;
-	uint8_t tx_buffer[1];
-	tx_buffer[0] = reg;
+	uint8_t tx_buffer[2];
+	tx_buffer[0] = ADXL362_READ_REG;
+	tx_buffer[1] = reg;
 
 	struct spi_buf tx_spi_buf = {
-		.buf = &tx_buffer,
+		.buf = tx_buffer,
 		.len = sizeof(tx_buffer)};
 
 	struct spi_buf_set spi_tx_buffer_set = {
@@ -85,22 +145,16 @@ static int readRegister(uint8_t reg, uint8_t* values, uint8_t size)
 		.buffers = &rx_spi_buf,
 		.count = 1};
 
-	// gpio pin set
-	gpio_pin_set(cs_gpio_dev,ADXL362_CS_GPIO_PIN,0);
-
-	err = spi_write(adxl1362_sens, &spi_cfg, &spi_tx_buffer_set);
-	if (err < 0)
+	err = spi_transceive(adxl1362_sens, &spi_cfg, &spi_tx_buffer_set, &spi_rx_buffer_set);
+	if (err)
 	{
-		printk("SPI write failed with error %d\n", err);
-		return 0;
+		printk("SPI error: %d\n", err);
 	}
-	err = spi_read(adxl1362_sens, &spi_cfg, &spi_rx_buffer_set);
-	gpio_pin_set(cs_gpio_dev,ADXL362_CS_GPIO_PIN,1);
-
-	if (err < 0)
+	else
 	{
-		printk("SPI read failed with error %d\n", err);
-		return 0;
+		/* Connect MISO to MOSI for loopback */
+		printk("TX sent: %x\n", tx_buffer[0]);
+		printk("RX recv: %x\n", values[0]);
+		tx_buffer[0]++;
 	}
-	
 }
