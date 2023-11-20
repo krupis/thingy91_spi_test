03be94fba11ae6e2d250f88435d5f709bcd4c6c9
Lukas Petrikas <lukas.petrikas@teltonika.lt>
2023-11-20 15:01:02 +0200

    test

--- src/main.c
+++ src/main.c
@@ -1,38 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/spi.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
 
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main);
+#define MY_SPI_MASTER DT_NODELABEL(my_spi_master)
 
-#define ADXL362_WRITE_REG 0x0A
-#define ADXL362_READ_REG 0x0B
-#define ADXL362_READ_FIFO 0x0D
+#define DEVICE_ID 0xAD
 
-#define ADXL362_REG_DEVID_AD 0x00
-#define ADXL362_REG_DEVID_MST 0x01
-#define ADXL362_REG_PARTID 0x02
-
-
-static int readRegister(uint8_t reg, uint8_t *values, uint8_t size);
-
-
-
-
-// #define DEFAULT_ADXL362_NODE DT_ALIAS(adxl362)
-// BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_ADXL362_NODE, okay),
-// 			 "ADXL362 not specified in DT");
-
-// // DEVICE TREE STRUCTURE
-// const struct device *adxl1362_sens = DEVICE_DT_GET(DEFAULT_ADXL362_NODE);
-
-
-
-#define MY_SPI_MASTER DT_ALIAS(adxl362)
+// SPI master functionality
 const struct device *spi_dev;
 
 struct spi_cs_control spim_cs = {
@@ -41,101 +17,88 @@ struct spi_cs_control spim_cs = {
 };
 
 static const struct spi_config spi_cfg = {
-	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB |
-				 SPI_MODE_CPOL | SPI_MODE_CPHA,
+	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
 	.frequency = 4000000,
 	.slave = 0,
 	.cs = &spim_cs,
 };
 
-
-
-
-struct spi_dt_spec spec = SPI_DT_SPEC_GET(DT_NODELABEL(adxl362), SPI_WORD_SET(8) | SPI_MODE_GET(0), 1);
-
-int main(void)
+static void spi_init(void)
 {
-	int err;
-	printk("Program started \n");
-	if (spi_is_ready(&spec) == true){
-		printk("SPI is ready \n");
-	}
-	else{
-		printk("SPI is not ready \n");
-	}
-
-
-
 	spi_dev = DEVICE_DT_GET(MY_SPI_MASTER);
 	if(!device_is_ready(spi_dev)) {
 		printk("SPI master device not ready!\n");
 	}
 	else{
-		printk("SPI master device is ready!\n");
+		printk("SPI master device ready!\n");
+	}
+	
+	if(!device_is_ready(spim_cs.gpio.port)){
+		printk("SPI master chip select device not ready!\n");
+	}
+	else{
+		printk("SPI master chip select device ready!\n");
+	}
+}
+
+
+
+
+static int spi_test(void)
+{
+	static uint8_t tx_buffer[3];
+	static uint8_t rx_buffer[3];
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = rx_buffer,
+		.len = sizeof(rx_buffer),
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1
+	};
+
+
+	/*
+	first 2 bytes of tx specify read/write action and on which register to perform
+	last byte of rx contains the respons from the accelerometer
+	*/
+	tx_buffer[0] = 0x0B;
+	tx_buffer[1] = 0x00;
+
+	int error = spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+	if(error != 0){
+		printk("SPI transceive error: %i\n", error);
+		return error;
 	}
 
 
-
-
-	uint8_t values[1];
-	while (1)
+	if(DEVICE_ID == rx_buffer[2])
 	{
-		int ret = readRegister(ADXL362_REG_DEVID_AD, values, 1);
-		if (ret == 0)
-		{
-			printk("Read chip ID failed \n");
-			k_msleep(1000);
-		}
-		else
-		{
-			printk("Register chip ID:%.2x\n", values[0]);
-			k_msleep(1000);
-		}
+		printk("Successfull ACC communication\n");
 	}
+	else
+	{
+		printk("Faild ACC communication\n");
+	}
+
 	return 0;
 }
 
 
-//According to the ADXL362 datasheet (https://www.analog.com/media/en/technical-documentation/data-sheets/adxl362.pdf)
-//Figure 36, we follow multi byte structure where first byte is ADXL362_READ_REG and second byte is the register address
-static int readRegister(uint8_t reg, uint8_t *values, uint8_t size)
+int main(void)
 {
-	int err;
-	uint8_t tx_buffer[2];
-	tx_buffer[0] = ADXL362_READ_REG;
-	tx_buffer[1] = reg;
-
-	struct spi_buf tx_spi_buf = {
-		.buf = tx_buffer,
-		.len = sizeof(tx_buffer)};
-
-	struct spi_buf_set spi_tx_buffer_set = {
-		.buffers = &tx_spi_buf,
-		.count = 1};
-
-	struct spi_buf rx_spi_buf = {
-		.buf = values,
-		.len = size};
-
-	struct spi_buf_set spi_rx_buffer_set = {
-		.buffers = &rx_spi_buf,
-		.count = 1};
-
-	err = spi_transceive(spi_dev, &spi_cfg, &spi_tx_buffer_set, &spi_rx_buffer_set);
-	//err = spi_transceive_dt(&spec, &spi_tx_buffer_set, &spi_rx_buffer_set);
-	//err = spi_transceive(spi_dev_test, &spi_cfg, &spi_tx_buffer_set, &spi_rx_buffer_set);
-	if (err)
-	{
-		printk("SPI error: %d\n", err);
-		return 0;
-	}
-	else
-	{
-		/* Connect MISO to MOSI for loopback */
-		printk("TX sent[0]: %.2x\n", tx_buffer[0]);
-		printk("TX sent[1]: %.2x\n", tx_buffer[1]);
-		printk("RX recv: %.2x\n", values[0]);
-		tx_buffer[0]++;
-		return 1;
-	}
-}
+	printk("Start program \n");
+	spi_init();
+	spi_test();
+	return 0;
+}	
